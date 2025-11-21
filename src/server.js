27eab2971639b036379f21b5987cc2baf332d19a
Franco Palavicino <fppalavicino@miuandes.cl>
2025-11-20 21:35:29 -0300

    node.js + express backend structure

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import uploadRoutes from './routes/upload.routes.js';   
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/uploads', express.static('src/public/uploads'));
+app.use('/api', uploadRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Backend running on http://localhost:${PORT}`);
+});
