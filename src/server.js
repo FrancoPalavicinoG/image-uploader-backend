7dfe2acf7c9ea83ee1ec9392993c0dcfcf3118b7
Franco Palavicino <fppalavicino@miuandes.cl>
2025-11-21 22:44:16 -0300

    fix: deploy api

--- src/server.js
+++ src/server.js
@@ -8,7 +8,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: [
+      "http://localhost:5173",
+      "https://fascinating-arithmetic-f23bd5.netlify.app"
+    ]
+  }));
 app.use(morgan('dev'));
 app.use(express.json());
 
